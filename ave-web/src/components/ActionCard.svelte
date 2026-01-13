481790c7ff2be519ae02fafd30c826c23e2109bd
Artemas <arden@lanth.me>
2026-01-13 02:14:32 +0100

    you already know

--- ave-web/src/components/ActionCard.svelte
+++ ave-web/src/components/ActionCard.svelte
@@ -21,18 +21,18 @@
         <p class="text-[#878787] text-xs md:text-[18px]">{description}</p>
     </div>
 
-    <div class="flex flex-row gap-1.5 md:gap-[20px] min-h-[50px] md:min-h-full">
+    <div class="flex flex-row gap-1.5 md:gap-[20px] min-h-[60px] md:min-h-full">
         {#each buttons as button}
             <button
-                    class="flex flex-row items-center justify-center flex-1 md:flex-none md:aspect-square w-fit h-full gap-2 md:gap-[10px] px-4 md:px-[40px] py-2 md:py-[10px] bg-[#171717] text-[18px] hover:bg-[#202020] transition-colors duration-300 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                    class="flex flex-row items-center justify-center flex-1 md:flex-none md:aspect-square w-fit h-full gap-2 md:gap-[10px] px-4 md:px-[40px] py-3 md:py-[10px] bg-[#171717] text-[18px] hover:bg-[#202020] transition-colors duration-300 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
                     style="color: {button.color};"
                     on:click={button.onClick}
                     disabled={button.loading || button.disabled}
             >
                 {#if button.loading}
-                    <div class="w-5 h-5 md:w-[24px] md:h-[24px] border-2 border-current border-t-transparent rounded-full animate-spin"></div>
+                    <div class="w-4 h-4 md:w-[24px] md:h-[24px] border-2 border-current border-t-transparent rounded-full animate-spin"></div>
                 {:else}
-                    <img src={button.icon} alt="icon" class="w-5 h-5 md:w-auto md:h-auto" />
+                    <img src={button.icon} alt="icon" class="w-4 h-4 md:w-auto md:h-auto" />
                 {/if}
             </button>
         {/each}
